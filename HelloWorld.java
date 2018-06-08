c59a1d42d2794e46b1f63f46652c1f40ac3c4d39
muratoeztuerk <40055791+muratoeztuerk@users.noreply.github.com>
2018-06-08 10:19:17 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,17 @@
 
+/**
+ * @author Murat Öztürk
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String str = "Hello World!)"; // Stringliteral
+        System.out.println(str); // Stringliteral ausgeben
+
+    }
+
+}
