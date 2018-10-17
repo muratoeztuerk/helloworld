c2fb931d952743a28e26592c14b25b4810ab8b66
Murat Öztürk <40055791+muratoeztuerk@users.noreply.github.com>
2018-10-17 03:00:15 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,17 @@
-
 /**
  * @author Murat Öztürk
  */
 public class HelloWorld {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+     	String str = "Hello World! :)"; // eine Referenzvariable vom Typ String deklarieren und ein Wert zuweissen.
+		int counter = 1; // eine pirimitive Variable vom Typ int deklarieren und integerliteral 1
+						// zuweissen
+		while (counter <= 5) { // eine Schleife, um oben zugewiesene Stringiteral "Hello World" 5 mal
+							  // auszugeben
 
-        String str = "Hello World! :)"; // Stringliteral
-        System.out.println(str); // Stringliteral ausgeben
-
+		System.out.println(str + " - " + counter); // Referenz- und pirimitive Variable ausgeben
+		counter++; // Zähler inkrementieren
+		}
+        
     }
-
 }
